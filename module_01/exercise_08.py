726fa86533a5d3448b0511b774311085a7d21f3d
Dave McCollough <davemccollough@gmail.com>
2026-05-05 19:55:36 -0600

    Exercise 7,8 and 9

---
+++ module_01/exercise_08.py
@@ -0,0 +1,14 @@
+# Exercise 8: Formatting Strings Manually
+
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Mary had a little lamb",
+    "its fleece as white as snow",
+    "and everywhere Mary went",
+    "the lmab was sure to go",
+))
---
+++ module_01/exercise_09.py
@@ -0,0 +1,16 @@
+# Exercise 9: Multiline Strings
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSept\nOct\nNov\nDec"
+
+print("Here the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+      Multine
+      strings
+      go 
+      in   
+      triple 
+      quotes.
+      """)
